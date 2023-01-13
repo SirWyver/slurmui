d30a7af6827e1a2d9d4d89e1a74b32bf95900e8c
normanm <norman.mueller@tum.de>
2023-01-13 19:41:57 +0100

    gpu monitoring

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slurmui",
-    version="0.1.0",
+    version="0.2.0",
     author="Norman Müller",
     author_email="norman.mueller@tum.de",
     url="https://github.com/SirWyver/slurmui",
--- slurmui/slurmui.py
+++ slurmui/slurmui.py
@@ -30,12 +30,107 @@ SQUEUE_DEBUG = """
             190649 su                              triplane0_l bob  RUNNING    3:03:17 4-00:00:00    1 valinor
 """
 
+SINFO_DEBUG = """
+HOSTNAMES           GRES                                              GRES_USED
+andram              gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+andram              gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+angmar              gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+angmar              gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+balar               gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+balar               gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+balrog              gpu:rtx_3090:8,mps:rtx_3090:800                   gpu:rtx_3090:8(IDX:0-7),mps:rtx_3090:0(IDX:N/A)
+balrog              gpu:rtx_3090:8,mps:rtx_3090:800                   gpu:rtx_3090:8(IDX:0-7),mps:rtx_3090:0(IDX:N/A)
+char                gpu:gtx_1080:4,mps:gtx_1080:800                   gpu:gtx_1080:4(IDX:0-3),mps:gtx_1080:0(IDX:N/A)
+char                gpu:gtx_1080:4,mps:gtx_1080:800                   gpu:gtx_1080:4(IDX:0-3),mps:gtx_1080:0(IDX:N/A)
+daidalos            gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+daidalos            gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+doriath             gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+doriath             gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+erebor              gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+erebor              gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+eriador             gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+eriador             gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+falas               gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+falas               gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+gimli               gpu:rtx_3090:8,mps:rtx_3090:800                   gpu:rtx_3090:8(IDX:0-7),mps:rtx_3090:0(IDX:N/A)
+gimli               gpu:rtx_3090:8,mps:rtx_3090:800                   gpu:rtx_3090:8(IDX:0-7),mps:rtx_3090:0(IDX:N/A)
+gondor              gpu:rtx_2080:9,mps:rtx_2080:900                   gpu:rtx_2080:9(IDX:0-8),mps:rtx_2080:0(IDX:N/A)
+gondor              gpu:rtx_2080:9,mps:rtx_2080:900                   gpu:rtx_2080:9(IDX:0-8),mps:rtx_2080:0(IDX:N/A)
+himring             gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+himring             gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+hithlum             gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+hithlum             gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+ikarus              gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+ikarus              gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+lothlann            gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:7(IDX:1-7),mps:rtx_2080:0(IDX:N/A)
+lothlann            gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:7(IDX:1-7),mps:rtx_2080:0(IDX:N/A)
+moria               gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:8(IDX:0-7),mps:rtx_2080:0(IDX:N/A)
+moria               gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:8(IDX:0-7),mps:rtx_2080:0(IDX:N/A)
+pegasus             gpu:gtx_1080:8,mps:gtx_1080:800                   gpu:gtx_1080:0(IDX:N/A),mps:gtx_1080:0(IDX:N/A)
+pegasus             gpu:gtx_1080:8,mps:gtx_1080:800                   gpu:gtx_1080:0(IDX:N/A),mps:gtx_1080:0(IDX:N/A)
+ramdal              gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+ramdal              gpu:a100:4,mps:a100:400                           gpu:a100:4(IDX:0-3),mps:a100:0(IDX:N/A)
+seti                gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:0(IDX:N/A),mps:rtx_2080:0(IDX:N/A)
+seti                gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:0(IDX:N/A),mps:rtx_2080:0(IDX:N/A)
+sorona              gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:8(IDX:0-7),mps:rtx_2080:0(IDX:N/A)
+sorona              gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:8(IDX:0-7),mps:rtx_2080:0(IDX:N/A)
+tarsonis            gpu:gtx_1080:4,mps:gtx_1080:400                   gpu:gtx_1080:0(IDX:N/A),mps:gtx_1080:0(IDX:N/A)
+tarsonis            gpu:gtx_1080:4,mps:gtx_1080:400                   gpu:gtx_1080:0(IDX:N/A),mps:gtx_1080:0(IDX:N/A)
+umoja               gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:7(IDX:0-4,6-7),mps:rtx_2080:0(IDX:N/A
+umoja               gpu:rtx_2080:8,mps:rtx_2080:800                   gpu:rtx_2080:7(IDX:0-4,6-7),mps:rtx_2080:0(IDX:N/A
+valinor             gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+valinor             gpu:rtx_a6000:8,mps:rtx_a6000:800                 gpu:rtx_a6000:8(IDX:0-7),mps:rtx_a6000:0(IDX:N/A)
+"""
+
 def sqeue_debug():
     SQUEUE_DEBUG2 = re.sub(' +', ' ', SQUEUE_DEBUG)
     data = io.StringIO(SQUEUE_DEBUG2)
     df = pd.read_csv(data, sep=" ")
     del df[df.columns[0]]
     return df
+
+def parse_gres_used(gres_used_str):
+    _,device,num_gpus,alloc_str = re.match("(.*):(.*):(.*)\(IDX:(.*)\),.*",gres_used_str).groups()
+    num_gpus = int(num_gpus)
+    alloc_gpus = []
+    for gpu_ids in alloc_str.split(","):
+        if "-" in gpu_ids:
+            start, end = gpu_ids.split("-")
+            for i in range(int(start), int(end)+1):
+                alloc_gpus.append(i)
+        else:
+            if gpu_ids == "N/A":
+                alloc_gpus = list(range(num_gpus))
+            else:
+                alloc_gpus.append(int(gpu_ids))
+            
+    return {"Device": device,
+            "#Alloc": num_gpus,
+            "Free IDX": [idx for idx in range(num_gpus) if idx not in alloc_gpus]}
+
+
+def parse_gres(gres_str):
+    _,device,num_gpus = re.match("(.*):(.*):(.*),.*",gres_str).groups()
+    num_gpus = int(num_gpus)
+    return {"Device": device,
+            "#Total": num_gpus}
+
+def sinfo_debug():
+    SINFO_DEBUG2 = re.sub(' +', ' ', SINFO_DEBUG)
+    data = io.StringIO(SINFO_DEBUG2)
+    df = pd.read_csv(data, sep=" ")
+    overview_df = [ ]# pd.DataFrame(columns=['Host', "Device", "#Avail", "#Total", "Free IDX"])
+    for row in df.iterrows():
+        host_info = parse_gres(row[1]['GRES'])
+        host_avail_info = parse_gres_used(row[1]['GRES_USED'])
+        host_info.update(host_avail_info)
+        host_info["#Avail"] = host_info['#Total'] - host_info["#Alloc"]
+        host_info['Host'] = str(row[1]["HOSTNAMES"])
+        overview_df.append(host_info)
+    overview_df = pd.DataFrame.from_records(overview_df).drop_duplicates("Host")
+    overview_df = overview_df[['Host', "Device", "#Avail", "#Total", "Free IDX"]]
+    return overview_df
+
 
 DEBUG = False
 
@@ -45,15 +140,20 @@ class SlurmUI(App):
 
     BINDINGS = [
         ("d", "stage_delete", "Delete job"),
-        ("r", "refresh_table", "Refresh"),
+        ("r", "refresh", "Refresh"),
+        ("s", "sort", "Sort"),
         ("l", "display_log", "Log"),
+        ("g", "display_gpu", "GPU"),
         ("q", "abort_quit", "Quit"),
         ("enter", "confirm", "Confirm"),
         ("escape", "abort_quit", "Abort"),  
         # ("k", "scroll_up", "Up")    
 
     ]
-    STAGE = None
+    STAGE = {"action": "monitor"} 
+    gpu_overview_df = None
+    sqeue_df =None
+
     def compose(self) -> ComposeResult:
         self.header = Header()
         self.footer = Footer()
@@ -63,24 +163,38 @@ class SlurmUI(App):
         yield Container(self.table, self.txt_log)
         yield self.footer
 
-    def update_squeue_table(self, set_colums=False):
-        self.table.clear()
+    
+    def query_squeue(self, sort_column=None, sort_ascending=True):
         squeue_df = get_squeue() if not DEBUG else sqeue_debug()
-        if set_colums:
-            self.table.add_columns(*squeue_df.columns)
+        if sort_column is not None:
+            squeue_df = squeue_df.sort_values(squeue_df.columns[sort_column],ascending=sort_ascending)
+        self.sqeue_df = squeue_df
+        return squeue_df
+
+    def update_squeue_table(self, sort_column=None, sort_ascending=True):
+        self.table.clear()
+        squeue_df = self.query_squeue(sort_column=sort_column, sort_ascending=sort_ascending)
+        self.table.columns = []
+        self.table.add_columns(*squeue_df.columns)
         for row in squeue_df.iterrows():
             table_row = [str(x) for x in row[1].values]
             self.table.add_row(*table_row)
         self.table.focus()
 
-    def action_refresh_table(self):
-        self.update_squeue_table()
+    def action_refresh(self):
+        if self.STAGE["action"] == "monitor":
+            self.update_squeue_table()
+        elif self.STAGE["action"] == "log":
+            self.update_log(self.STAGE["job_id"])
+        elif self.STAGE["action"] == "gpu":
+            self.update_gpu_table()
 
     def on_mount(self):
         self._minimize_text_log()
 
     def on_ready(self) -> None:
-        self.update_squeue_table(set_colums=True)
+        self.update_squeue_table()
+        self.query_gpus()
 
     # def action_modal(self):
     #     log_screen = LogScreen()
@@ -113,21 +227,79 @@ class SlurmUI(App):
 
     def action_display_log(self):
         job_id, job_name = self._get_selected_job()
+        self.STAGE = {"action": "log", "job_id": job_id, "job_name": job_name}
+        self._maximize_text_log()
+        try:
+            self.update_log(job_id)
+        except Exception as e:
+            self.txt_log.clear()
+            self.txt_log.write(str(e))
+
+    def query_gpus(self,  sort_column=None, sort_ascending=True):
+        overview_df = get_sinfo() if not DEBUG else sinfo_debug()
+        if sort_column is not None:
+            overview_df = overview_df.sort_values(overview_df.columns[sort_column],ascending=sort_ascending)
+        
+        self.gpu_overview_df = overview_df
+        # also change the title to include GPU information
+        total_num_gpus = overview_df["#Total"].sum()
+        total_available = overview_df["#Avail"].sum()
+        self.title = f"SlurmUI --- GPU STATS: {total_available}/{total_num_gpus}"
+        return overview_df
+
+
+    def update_gpu_table(self, sort_column=None, sort_ascending=True):
+        self.table.clear()
+        self.table.columns = []
+        overview_df = self.query_gpus(sort_column=sort_column, sort_ascending=sort_ascending)
+        self.table.add_columns(*overview_df.columns)
+        for row in overview_df.iterrows():
+            table_row = [str(x) for x in row[1].values]
+            self.table.add_row(*table_row)
+        self.table.focus()
+
+
+
+    def action_display_gpu(self):
+        self.STAGE = {"action": "gpu"}
+        try:
+            self.update_gpu_table()
+        except Exception as e:
+            self.txt_log.clear()
+            self.txt_log.write(str(e))
+
+    def action_sort(self):
+        selected_column = self.table.cursor_cell
+        column_idx = selected_column.column
+        if column_idx != self.STAGE.get("column_idx"):
+            self.STAGE["sort_ascending"] = False
+        else:
+            self.STAGE["sort_ascending"] = not self.STAGE.get("sort_ascending",True)
+        self.STAGE['column_idx'] = column_idx
+        if self.STAGE["action"] == "monitor":
+            self.update_squeue_table(sort_column=column_idx, sort_ascending=self.STAGE["sort_ascending"])
+        elif self.STAGE["action"] == "gpu":
+            self.update_gpu_table(sort_column=column_idx, sort_ascending=self.STAGE["sort_ascending"])
+
+
+
+    def update_log(self, job_id):
         if not DEBUG:
             log_fn = get_log_fn(job_id)
             txt_lines = read_log(log_fn)
         else:
             txt_lines = read_log("~/ram_batch_triplane0_l1.txt")
 
-        self._maximize_text_log()
         self.txt_log.clear()
         for text_line in txt_lines:
             self.txt_log.write(text_line)
 
-        self.STAGE = {"action": "log", "job_id": job_id, "job_name": job_name}
+
 
     def action_confirm(self):
-        if self.STAGE is not None:
+        if self.STAGE["action"] == "monitor":
+            pass
+        else:
             self.txt_log.clear()
             # job to delete
             if self.STAGE["action"] == "delete":
@@ -138,14 +310,15 @@ class SlurmUI(App):
 
 
     def action_abort(self):
-        if self.STAGE is not None:
-            if self.STAGE["action"] == "log":
-                self._minimize_text_log()
-            self.txt_log.clear()
-            self.STAGE = None
+        if self.STAGE["action"] == "log":
+            self._minimize_text_log()
+        elif self.STAGE["action"] == "gpu":
+            self.update_squeue_table()
+        self.txt_log.clear()
+        self.STAGE['action'] = "monitor"
 
     def action_abort_quit(self):
-        if self.STAGE is None:
+        if self.STAGE["action"] == "monitor":
             # self.emit_no_wait(message=Message())
             self.action_quit()
         else:
@@ -154,6 +327,21 @@ class SlurmUI(App):
 
 def perform_scancel(job_id):
     os.system(f"""scancel {job_id}""")
+
+
+def get_sinfo():
+    response_string = subprocess.check_output("""sinfo --Node -O "NodeHost,GresUsed:200""", shell=True).decode("utf-8")
+    formatted_string = re.sub(' +', ' ', response_string)
+    data = io.StringIO(formatted_string)
+    df = pd.read_csv(data, sep=" ")
+    overview_df = [ ]# pd.DataFrame(columns=['Host', "Device", "#Avail", "#Total", "Free IDX"])
+    for row in df.iterrows():
+        host_info = parse_gres_used(row[1]['GRES_USED'])
+        host_info['Host'] = str(row[1]["HOSTNAMES"])
+        overview_df.append(host_info)
+    overview_df = pd.DataFrame.from_records(overview_df).drop_duplicates("Host")
+    overview_df = overview_df[['Host', "Device", "#Avail", "#Total", "Free IDX"]]
+    return overview_df
 
 
 def get_squeue():
