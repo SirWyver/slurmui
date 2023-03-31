cd8a82445edfa0dea4aa54557d8c47ec6a713440
normanm <norman.mueller@tum.de>
2023-03-31 16:03:04 +0200

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slurmui",
-    version="0.2.8",
+    version="0.2.9",
     author="Norman Müller",
     author_email="norman.mueller@tum.de",
     url="https://github.com/SirWyver/slurmui",
