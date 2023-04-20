ae491d08ca8be4e2159949ba2ed66a814798fddb
normanm <norman.mueller@tum.de>
2023-04-20 15:17:15 +0200

    textual version update

--- slurmui/slurmui.py
+++ slurmui/slurmui.py
@@ -37,6 +37,7 @@ class SlurmUI(App):
         self.header = Header()
         self.footer = Footer()
         self.table = DataTable( id="table")
+        self.table.zebra_stripes = True
         self.txt_log = TextLog(wrap=True, highlight=True, id="info")
         yield self.header
         yield Container(self.table, self.txt_log)
@@ -51,14 +52,14 @@ class SlurmUI(App):
         return squeue_df
 
     def update_squeue_table(self, sort_column=None, sort_ascending=True):
-        self.table.clear()
+        self.table.clear(columns=True)
         squeue_df = self.query_squeue(sort_column=sort_column, sort_ascending=sort_ascending)
         # add device information
         squeue_df["GPU_IDS"] = "N/A"
         real_session_mask = squeue_df["PA"]!="in"
         if real_session_mask.any():
-            squeue_df["GPU_IDS"][real_session_mask] = "bobo" #squeue_df[real_session_mask]["JOBID"].apply(lambda x: get_job_gpu_ids(x))
-        self.table.columns = []
+            squeue_df["GPU_IDS"][real_session_mask] =squeue_df[real_session_mask]["JOBID"].apply(lambda x: get_job_gpu_ids(x))
+        # self.table.columns = []
         self.table.add_columns(*squeue_df.columns)
         for row in squeue_df.iterrows():
             table_row = [str(x) for x in row[1].values]
@@ -141,8 +142,7 @@ class SlurmUI(App):
 
 
     def update_gpu_table(self, sort_column=None, sort_ascending=True):
-        self.table.clear()
-        self.table.columns = []
+        self.table.clear(columns=True)
         overview_df = self.query_gpus(sort_column=sort_column, sort_ascending=sort_ascending)
         self.table.add_columns(*overview_df.columns)
         for row in overview_df.iterrows():
@@ -161,8 +161,8 @@ class SlurmUI(App):
             self.txt_log.write(str(e))
 
     def action_sort(self):
-        selected_column = self.table.cursor_cell
-        column_idx = selected_column.column
+        # selected_column = self.table.cursor_cell
+        column_idx = self.table.cursor_column
         if column_idx != self.STAGE.get("column_idx"):
             self.STAGE["sort_ascending"] = False
         else:
@@ -173,6 +173,7 @@ class SlurmUI(App):
         elif self.STAGE["action"] == "gpu":
             self.update_gpu_table(sort_column=column_idx, sort_ascending=self.STAGE["sort_ascending"])
 
+        self.table.cursor_coordinate  = (0,column_idx)
 
 
     def update_log(self, job_id):
@@ -287,7 +288,7 @@ def get_squeue():
 
 def get_job_gpu_ids(job_id):
     try:
-        response_string = subprocess.check_output(f"""srun --jobid {job_id} '/bin/env' | grep SLURM_STEP_GPUS""", shell=True).decode("utf-8")
+        response_string = subprocess.check_output(f"""srun --jobid {job_id} '/bin/env' | grep SLURM_STEP_GPUS""", shell=True,stderr=subprocess.STDOUT).decode("utf-8")
         formatted_string = response_string.split("=")[-1].strip()
     except:
         return "N/A"
